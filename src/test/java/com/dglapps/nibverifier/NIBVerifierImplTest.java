d84240ab456aed698d9ca4d185a3bd9144122303
Daniel Gomez <daniels26@gmail.com>
2020-11-22 22:43:35 +0100

    Upgrade gradlew and libraries

--- src/test/java/com/dglapps/nibverifier/NIBVerifierImplTest.java
+++ src/test/java/com/dglapps/nibverifier/NIBVerifierImplTest.java
@@ -2,59 +2,60 @@ package com.dglapps.nibverifier;
 
 import com.dglapps.nibverifier.domain.NIBVerifier;
 import com.dglapps.nibverifier.domain.NIBVerifierImpl;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NIBVerifierImplTest {
 	
 	private NIBVerifier verifier;
 	
-	@Before
+	@BeforeEach
 	public void setup() {
 		verifier = new NIBVerifierImpl();
 	}
 	
 	@Test
 	public void checkInvalidNonDigitNib() {
-		Assert.assertFalse(verifier.checkNib("23243C234234"));
+		assertFalse(verifier.checkNib("23243C234234"));
 	}
 	
 	@Test
 	public void checkNullNib() {
-		Assert.assertFalse(verifier.checkNib(null));
+		assertFalse(verifier.checkNib(null));
 	}
 	
 	@Test
 	public void checkInvalidWrongLengthNib() {
-		Assert.assertFalse(verifier.checkNib("2234234"));
+		assertFalse(verifier.checkNib("2234234"));
 	}
 	
 	@Test
 	public void checkValidNib() {
-		Assert.assertTrue(verifier.checkNib("223423412345676543268"));
+		assertTrue(verifier.checkNib("223423412345676543268"));
 	}
 	
 	@Test
 	public void getControlInvalidNonDigitNib() {
-		Assert.assertEquals(0, verifier.getControl("23243C234234"));
+		assertEquals(0, verifier.getControl("23243C234234"));
 	}
 	
 	@Test
 	public void getControlNullNib() {
-		Assert.assertEquals(0, verifier.getControl(null));
+		assertEquals(0, verifier.getControl(null));
 	}
 	
 	@Test
 	public void getControlInvalidWrongLengthNib() {
-		Assert.assertEquals(0, verifier.getControl("2234234"));
+		assertEquals(0, verifier.getControl("2234234"));
 	}
 	
 	@Test
 	public void getControlValidNib() {
-		Assert.assertEquals(68, verifier.getControl("223423412345676543268"));
-		Assert.assertEquals(9, verifier.getControl("2234234123456715432"));
-		Assert.assertEquals(9, verifier.getControl("223423412345671543209"));
+		assertEquals(68, verifier.getControl("223423412345676543268"));
+		assertEquals(9, verifier.getControl("2234234123456715432"));
+		assertEquals(9, verifier.getControl("223423412345671543209"));
 	}
 	
 }
