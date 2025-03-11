e7d2bc0c791c7b382e7411b41ac66a6653c36679
Daniel Gómez <daniels26@gmail.com>
2025-03-11 22:19:26 +0100

    use junit bom to specify version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,9 @@ repositories {
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.17")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation("org.junit:junit-bom:5.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.assertj:assertj-core:3.27.3")
 }
 
